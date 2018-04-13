96ab72ca466171a9617eaa05f726f04f66ce496b
xy <545757460@qq.com>
2018-04-13 10:57:00 +0800

    1111

---
+++ app/src/main/java/szz3/shortvideo/com/testgit/MainActivity.java
@@ -0,0 +1,13 @@
+package szz3.shortvideo.com.testgit;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
